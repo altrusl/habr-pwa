dc8f39450cd4a877c85d799160617a89d0482df9
ruslan <ruslan.makarov@gmail.com>
2018-08-08 09:25:13 +0700

    init

--- hello-habr/hh.js
+++ hello-habr/hh.js
@@ -1,14 +1,12 @@
 window.onload = function() {
-
     fetch("hh.txt").then(data => data.text()).then(data => {
-        // document.getElementById("text").innerHTML = data
         animateText(data)
       });
 }
 
 function animateText(data) {
     var ele = document.getElementById("text"),
-    txt = data.split("");
+        txt = data.split("");
     var interval = setInterval(function(){
     if(!txt[0]){
         return clearInterval(interval);
