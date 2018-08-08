127ed3cc6e339ffddf7adc4a7867da243e3f9732
ruslan <ruslan.makarov@gmail.com>
2018-08-08 09:22:16 +0700

    init

--- hello-habr/hh.js
+++ hello-habr/hh.js
@@ -1,10 +1,18 @@
 window.onload = function() {
+
+    fetch("hh.txt").then(data => data.text()).then(data => {
+        // document.getElementById("text").innerHTML = data
+        animateText(data)
+      });
+}
+
+function animateText(data) {
     var ele = document.getElementById("text"),
-	    txt = "Hello Hubr".split("");
-	var interval = setInterval(function(){
-		if(!txt[0]){
-			return clearInterval(interval);
-		};
-		ele.innerHTML += txt.shift();
-	}, 200);
+    txt = data.split("");
+    var interval = setInterval(function(){
+    if(!txt[0]){
+        return clearInterval(interval);
+    };
+    ele.innerHTML += txt.shift();
+    }, 200);
 }
